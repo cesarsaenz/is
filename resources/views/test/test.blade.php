f0f84a90d5492c909912d4dc10240a06897ed9a5
Cesar Saenz <cesar.saenz@gmail.com>
2015-07-01 18:36:07 -0700

    updated to use port 9999

--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -27,7 +27,7 @@
     <script src="https://cdn.socket.io/socket.io-1.2.0.js"></script>
     <script src="http://code.jquery.com/jquery-1.11.1.js"></script>
     <script>
-      var socket = io('localhost:9999');
+      var socket = io('9999');
       $('form').submit(function(){
         socket.emit('chat message', $('#m').val());
         $('#m').val('');
